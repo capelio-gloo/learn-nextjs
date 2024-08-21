e05fbc2840ae63733c1f9cead96025b47bbc303a
Corey Capel <ccapel@gloo.us>
2024-08-21 14:41:28 -0600

    Add initial app files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({weight: '400', style: 'normal', subsets: ['latin'] });
+export const inter = Inter({ subsets: ['latin'] });
